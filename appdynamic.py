2dda263696afc6493a3a25297bd4cc9aaef691fe
KHALEED\mulla <mullamohammadk123@gmail.com>
2025-07-02 19:25:38 +0530

    Set page config with title, icon, and layout in appdynamic.py

--- appdynamic.py
+++ appdynamic.py
@@ -3,3 +3,11 @@ from datetime import datetime
 import time
 import requests
 import json
+
+#Set page configuration
+st.set_page_config(
+    page_title="Simple Chatbot",
+    page_icon="🤖",
+    layout="centered",
+    # initial_sidebar_state="expanded"
+)
